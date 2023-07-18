91b32273433ba69e7dbf84284da7d12013afb6e5
Gleb <franklin777@list.ru>
2023-07-18 23:12:05 +0500

    вторая работа

---
+++ 2023.07.14/1_1.py
@@ -0,0 +1,16 @@
+num1 = int(input("введи первое число: "))
+num2 = int(input("введи второе число: "))
+num3 = int(input("введи третье число: "))
+if num1>=0: 
+    a = num1
+else: 
+    a = 0
+if num2>= 0:
+    b = num2
+else: 
+    b = 0
+if num3>= 0:
+    c = num3
+else: 
+    c = 0
+print(a+b+c) 
