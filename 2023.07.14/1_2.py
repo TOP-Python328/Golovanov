51f89cfde65d3ba33d20426dc826b3a487f8a001
Gleb <franklin777@list.ru>
2023-07-19 00:56:02 +0500

    удаление дряни

--- 2023.07.14/1_2.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("введи первое число: "))
-num2 = int(input("введи второе число: "))
-if num1%num2==0:
-   print(f"{num1} делится на {num2} нацело \n частное:{int(num1/num2)}")
-else: print(f"{num1} не делится на {num2} нацело \n неполное частное: {int(num1/num2)} \n остаток: {num1%num2}")
--- 2023.07.14/1_3.py
+++
@@ -1,4 +0,0 @@
-year = int(input("введи год: "))
-if (year%4==0 and year%100!= 0 or year%400==0):  
-    print("да")
-else: print("нет")
