31116955850df60a3462a85e23796b7d09eebf56
Gleb <franklin777@list.ru>
2023-07-25 23:43:10 +0500

    first task

--- 2023.07.21/1.py
+++ 2023.07.21/1.py
@@ -2,5 +2,7 @@ num = int(input("введи любое число: "))
 numRev = []
 while num%7==0: 
     numRev.append(num) 
+    num = int(input("Введите число: "))
 else: 
+    numRev.reverse()
     print(numRev)
